ab32f783f807ee73160c34dcb25592b2a89edd32
Sorin <jordache_sor@yahoo.com>
2021-05-22 11:45:35 +0300

    added browse and display in listview

--- Code Folders/MainWindow.xaml.cs
+++ Code Folders/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using Code_Folders.Models;
+using System.Collections.Generic;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Collections.ObjectModel;
 
 namespace Code_Folders
 {
@@ -7,9 +11,28 @@ namespace Code_Folders
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<CodeFolder> foldersList = new();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            foldersList.Add( new CodeFolder { Path = @"C:\Users\jorda\source\repos\Code Folders" } );
+            foldersList.Add( new CodeFolder { Path = @"C:\Users\jorda\source\repos" } );
+
+            foldersListView.ItemsSource = foldersList;
+        }
+
+        private void BrowseToFolderClick(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new()
+            {
+                IsFolderPicker = true
+            };
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                foldersList.Add(new CodeFolder { Path = dialog.FileName });
+            }
         }
     }
 }

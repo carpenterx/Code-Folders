c7c1b318baa2ee462d6cd7d153b7cb19213ee19d
Sorin <jordache_sor@yahoo.com>
2021-05-22 18:35:06 +0300

    added saving and loading

--- Code Folders/MainWindow.xaml.cs
+++ Code Folders/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using Code_Folders.Models;
-using System.Collections.Generic;
 using System.Windows;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Newtonsoft.Json;
+using System.IO;
+using System;
 
 namespace Code_Folders
 {
@@ -15,15 +17,25 @@ namespace Code_Folders
     public partial class MainWindow : Window
     {
         private ObservableCollection<CodeFolder> foldersList = new();
+        private static string APPLICATION_FOLDER = "Code Folders";
+        private static string APPLICATION_DATA_FILE = "data.json";
 
         public MainWindow()
         {
             InitializeComponent();
 
-            foldersList.Add( new CodeFolder { Path = @"C:\Users\jorda\source\repos\Code Folders" } );
-            foldersList.Add( new CodeFolder { Path = @"C:\Users\jorda\source\repos" } );
+            LoadFoldersList();
 
             foldersListView.ItemsSource = foldersList;
+        }
+
+        private void LoadFoldersList()
+        {
+            string dataFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), APPLICATION_FOLDER, APPLICATION_DATA_FILE);
+            if (File.Exists(dataFilePath))
+            {
+                foldersList = JsonConvert.DeserializeObject<ObservableCollection<CodeFolder>>(File.ReadAllText(dataFilePath));
+            }
         }
 
         private void BrowseToFolderClick(object sender, RoutedEventArgs e)
@@ -68,6 +80,18 @@ namespace Code_Folders
         private void OpenFolderPathInExplorer(CodeFolder codeFolder)
         {
             Process.Start("explorer.exe", codeFolder.Path);
+        }
+
+        private void SaveListOnClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(foldersList, Formatting.Indented);
+            string dataFileRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), APPLICATION_FOLDER);
+            if(!Directory.Exists(dataFileRoot))
+            {
+                Directory.CreateDirectory(dataFileRoot);
+            }
+            string dataFilePath = Path.Combine(dataFileRoot, APPLICATION_DATA_FILE);
+            File.WriteAllText(dataFilePath, json);
         }
     }
 }
